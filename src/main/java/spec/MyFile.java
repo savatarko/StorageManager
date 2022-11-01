3dca195a85d0ea3f851e15d6e61fb49b9c34666f
savatarko <ivkovic3@gmail.com>
2022-11-01 13:55:17 +0100

    myfile posix fix

--- src/main/java/spec/MyFile.java
+++ src/main/java/spec/MyFile.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.PosixFileAttributes;
 
 @Getter
@@ -22,9 +23,9 @@ public class MyFile {
     public MyFile(File file, String type) {
         this.file = file;
         this.type = type;
-        Path path = Paths.get(file.getPath());
+        Path path = Paths.get(file.getAbsolutePath().toString().replaceAll("\\\\", "/"));
         try {
-            PosixFileAttributes attrs = Files.readAttributes(path, PosixFileAttributes.class);
+            BasicFileAttributes attrs = Files.readAttributes(path, BasicFileAttributes.class);
             createtime = attrs.creationTime().toString();
             modtime = attrs.lastModifiedTime().toString();
             size = Long.toString(attrs.size());
@@ -32,9 +33,9 @@ public class MyFile {
         catch (Exception e)
         {
             e.printStackTrace();
-            createtime = "-1";
-            modtime = "-1";
-            size = "-1";
+            createtime = "0";
+            modtime = "0";
+            size = "0";
         }
     }
 
